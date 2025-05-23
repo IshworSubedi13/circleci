36be24e234c0db3558f1c9438303c603b0176bf9
Ishwor Subedi <ishworsubedi13@gmail.com>
2025-05-23 14:19:12 +0300

    test added for borrow.

---
+++ tests/test.py
@@ -0,0 +1,8 @@
+from book.books import Book
+
+
+def test_book_borrow_success():
+    book = Book("Clean Code", "Robert Cecil Martin")
+    success = book.borrow()
+    assert success is True
+    assert book.available is False
